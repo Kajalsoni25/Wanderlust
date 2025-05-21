ae0036539ecd8c599af411f614cd74b89a78bc8f
Kajal soni <kajalssoni25@gmail.com>
2025-05-21 14:43:28 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+.then(() => {
+    console.log("connected to DB");
+})
+.catch((err) => {
+    console.log(err);
+}); 
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    const listingsWithOwner = initData.data.map((obj) => ({
+        ...obj,
+        owner: "681a348ece5b0ea104b8ed6f",
+    }));
+    await Listing.insertMany(listingsWithOwner);
+    console.log("data was initialized");
+};
+
+initDB();
